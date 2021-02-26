9ced20a99d25dc2901e28fa79aac1e2430feecbc
Hallgeir Lien <hallgeir.lien@tieto.com>
2021-02-26 10:20:37 +0100

    Fix framework

--- deploy.ps1
+++ deploy.ps1
@@ -7,3 +7,5 @@ if (-not (Test-Path $PSScriptRoot\helm-output))
 
 helm upgrade --install --namespace kubedemo --create-namespace kubedemo-web $PSScriptRoot\helm-output\kubedemo-web-1.0.tgz
 helm upgrade --install --namespace kubedemo --create-namespace kubedemo-worker $PSScriptRoot\helm-output\kubedemo-worker-1.0.tgz
+
+kubectl get pods -n kubedemo
