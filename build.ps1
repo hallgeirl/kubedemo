11543d34d9e6a2d63348faa0c23ea232887ae0f1
Hallgeir Lien <hallgeir.lien@tieto.com>
2021-02-26 10:16:15 +0100

    fix repo name, debugging

--- build.ps1
+++ build.ps1
@@ -1,8 +1,12 @@
-Remove-Item -Recurse $PSScriptRoot\src\WebApp\obj
-Remove-Item -Recurse $PSScriptRoot\src\WorkerApp\obj
+# The following is moved into github actions. Uncomment to build locally.
+#Remove-Item -Recurse $PSScriptRoot\src\WebApp\obj
+#Remove-Item -Recurse $PSScriptRoot\src\WorkerApp\obj
 
-docker build $PSScriptRoot\src\WebApp -t hallgeirl/kubedemo-web:1.0
-docker build $PSScriptRoot\src\WorkerApp -t hallgeirl/kubedemo-worker:1.0
+#docker build $PSScriptRoot\src\WebApp -t hallgeirl/kubedemo-web:1.0
+#docker build $PSScriptRoot\src\WorkerApp -t hallgeirl/kubedemo-worker:1.0
 
-docker push hallgeirl/kubedemo-web:1.0
-docker push hallgeirl/kubedemo-worker:1.0
+#docker push hallgeirl/kubedemo-web:1.0
+#docker push hallgeirl/kubedemo-worker:1.0
+
+helm package helm/kubedemo-web --version 1.0 -d helm-output
+helm package helm/kubedemo-worker --version 1.0 -d helm-output
---
+++ deploy.ps1
@@ -0,0 +1,9 @@
+if (-not (Test-Path $PSScriptRoot\helm-output))
+{
+    Write-Host "helm-output not found. Building helm packages..."
+    . $PSScriptRoot\build.ps1
+}
+
+
+helm upgrade --install --namespace kubedemo --create-namespace kubedemo-web $PSScriptRoot\helm-output\kubedemo-web-1.0.tgz
+helm upgrade --install --namespace kubedemo --create-namespace kubedemo-worker $PSScriptRoot\helm-output\kubedemo-worker-1.0.tgz
